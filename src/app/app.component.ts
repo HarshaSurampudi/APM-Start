a6fce2f5e0c2cfe279a9de3a8cfa28612d364914
Harsha Surampudi <harsha.surampudi1997@gmail.com>
2020-07-25 12:48:41 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: '<h1>Harsha Surampudi</h1>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pageTitle: string  = 'Acme Product Management';
+}
